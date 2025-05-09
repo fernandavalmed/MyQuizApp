2d4bead7bef0341af5515263fbbc0cb67923a99b
Fernanda Valdivia <fernandavaldiviam09@gmail.com>
2025-05-09 13:00:18 -0600

    Add project files.

---
+++ MyQuizApp/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace MyQuizApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Question[] questions = new Question[]
+            {
+                new Question("What is the capital of Germany?", 
+                new string[] {"Paris", "Berlin", "London", "Madrid"},
+                1),
+                new Question("What is 2 + 2?",
+                new string[] {"3", "4", "5", "6"},
+                1),
+                new Question("Who wrote 'Hamlet'?",
+                new string[] {"Goethe", "Austen", "Sheakspeare", "Dickents"},
+                2)
+            };
+
+            Quiz myQuiz = new Quiz(questions);
+            myQuiz.StartQuiz();
+            Console.ReadLine();
+        }
+    }
+}
